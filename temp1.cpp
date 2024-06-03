0458547b2575d065e237ef40804872aa7c4beb0b
ekrsvms <vamshi.krishna@ericsson.com>
2024-06-03 22:42:44 +0200

    Add temperature scripts

---
+++ temp1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+
+  double tempf = 29;
+  double tempc;
+
+  tempc = (tempf - 32) / 1.8;
+
+  std::cout << "The temp is " << tempc << " degrees Celsius. \n";
+}
