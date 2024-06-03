a653c99e6397149475c9e9f955987ed02b4d6cfb
ekrsvms <vamshi.krishna@ericsson.com>
2024-06-03 22:36:58 +0200

    Add block letters script

---
+++ block_letters.cpp
@@ -0,0 +1,16 @@
+// Sonny Li
+// Fun Fact: I played guitar in a band called Attica.
+
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "  SSS   L     \n";
+  std::cout << " S   S  L     \n";
+  std::cout << " S      L     \n";
+  std::cout << "  SSS   L     \n";
+  std::cout << "     S  L     \n";
+  std::cout << " S   S  L     \n";
+  std::cout << "  SSS   LLLLL \n";
+}
